31bb6c15ebe0de650cc23048932698f23f983138
Henry Sachs <Henry.Sachs@deutschebahn.com>
2021-12-27 11:44:00 +0100

    feeling lucky

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,18 @@
 const { awscdk } = require('projen');
+const { ArrowParens } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'projenprettiertest',
+  eslint: true,
+  eslintOptions: { prettier: true },
+  prettier: true,
+  prettierOptions: {
+    arrowParens: ArrowParens.ALWAYS,
+    printWidth: 80,
+    singleQuote: true,
+  },
+  prettierIgnoreEnabled: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
@@ -10,4 +20,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   // packageName: undefined,  /* The "name" in package.json. */
   // release: undefined,      /* Add release management to this project. */
 });
+
+project.prettier.config.semi = true;
+
 project.synth();
